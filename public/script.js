1c406638151c0fabc5f9e3a93b09371e74dc90d6
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2026-06-29 00:36:00 +0530

    update the script .js

--- public/script.js
+++ public/script.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Initialize Socket.io Connection
     const socket = io();
-    // const socket = {}
 
     // Trigger model download when socket connects/reconnects
-    // socket.on('connect', () => {
-    //     socket.emit('trigger-model-download');
-    // });
+    socket.on('connect', () => {
+        socket.emit('trigger-model-download');
+        console.log("Emitted event 'trigger-model-download'")
+    });
 })
