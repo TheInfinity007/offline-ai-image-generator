0d05cbd8bc9c71e9b62b5e14be3a4aff6f2bfe3f
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2026-06-28 22:45:11 +0530

    add script.js

---
+++ public/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Initialize Socket.io Connection
+    const socket = io();
+    // const socket = {}
+
+    // Trigger model download when socket connects/reconnects
+    // socket.on('connect', () => {
+    //     socket.emit('trigger-model-download');
+    // });
+})
